3d298cda995267fe6aa477f46737c88281b54847
Kate <you@example.com>
2020-11-05 04:35:52 +0300

    initial commit

---
+++ computersystem/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ComputersystemConfig(AppConfig):
+    name = 'computersystem'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kate/Desktop/PYTHON/DJANGO/ComputerSystem/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
