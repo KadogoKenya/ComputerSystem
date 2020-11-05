252e9d51a858a740b7de27622a02af84480609ab
Kate <you@example.com>
2020-11-05 09:12:06 +0300

    creating forms

---
+++ computersystem/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Computer
+from users.models import Profile
+import cloudinary.uploader
+from cloudinary.models import CloudinaryField
+import cloudinary
+
+
+
+
+class ComputerForm(forms.ModelForm):
+    class Meta:
+        model = Computer
+        fields=['computer_name','Ip_address','Mac_address','user','location']
